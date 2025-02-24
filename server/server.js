bc95ec1f4becb281cd008ad86e16bf4dfbe5aac0
ayush-anilan <109529899+ayush-anilan@users.noreply.github.com>
2025-02-24 11:21:16 +0530

    small changes in backend and frontend

--- server/server.js
+++ server/server.js
@@ -73,6 +73,12 @@ app.use("/api/users", userRouter);
 app.use("/api/posts", postRouter);
 app.use("/api/comments", commentRouter);
 
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
 // app.use(errorHandler);
 
 app.listen(port, () => {
